880fa6a3ada0a3572b7ad731bb3777c6fe6cd9f7
refactoredjello <gil.mandler@gmail.com>
2017-11-24 16:11:04 -0500

    Allows editing of tasks onclick

--- client/src/components/TaskEntry.jsx
+++ client/src/components/TaskEntry.jsx
@@ -1,22 +1,46 @@
+
+//TODO - Update task on press enter 
+//TODO - Update task in taskList on App
 import React from 'react'
 
 export default class TaskEntry extends React.Component {
-  constructor() {
-    super()
+  constructor(props) {
+    super(props)
     this.state = {
-      editing: false
+      editing: false,
+      currentTaskDetail: this.props.task.taskDetail
     }
   }
   launchEditTask(){
-      this.setState({editing: !this.state.editing})
+    this.setState({editing: !this.state.editing})
+  }
+  updateTaskDetail(e){
+    this.setState({currentTaskDetail: e.target.value},
+       () => console.log(this.state.currentTaskDetail))
   }
   render() {
-    const { taskDetail, isCompleted } = this.props.task
+    const { isCompleted } = this.props.task
     const { updateChecked, idx } = this.props
-    let edit = this.state.editing
-    console.log(edit)
+
+    //Show and hide editable text field to edit task
+    let textField = <span onClick={this.launchEditTask.bind(this)}> {this.state.currentTaskDetail} </span> 
+    if (this.state.editing) {
+      textField = <input 
+                    type="text" 
+                    value={this.state.currentTaskDetail} 
+                    onChange={this.updateTaskDetail.bind(this)} 
+                    onMouseLeave={this.launchEditTask.bind(this)}
+                    onKeyPress={(e) => { 
+                      if (e.key === 'Enter') this.launchEditTask.call(this)
+                      } 
+                    }
+                  />
+    }
     return (
-      <div><span onClick={this.launchEditTask.bind(this)}> {taskDetail} </span> <input type="checkbox" checked={isCompleted} onChange={e => updateChecked(idx)} /></div>
+      <div>
+        {textField}
+        <input type="checkbox" checked={isCompleted} onChange={e => updateChecked(idx)} />
+      </div>
     )
   }
 }
